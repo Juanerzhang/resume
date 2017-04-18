7233c372ca1663399c08f7339d4a9f13caf5d0fc
Juanerzhang <1197189535@qq.com>
2017-04-18 17:46:52 +0800

    更新

---
+++ js/index.js
@@ -0,0 +1,29 @@
+/*
+* @Author: Juanerzhang
+* @Date:   2017-04-17 20:19:18
+* @Last Modified by:   Juanerzhang
+* @Last Modified time: 2017-04-17 21:23:14
+*/
+
+
+$(function(){
+	/*var disX=0,//按下的坐标
+		listL=0, //当前按下list的left值
+		w = exp_list.clientWidth,  //滚动的长度
+		len = 0,
+		n = 0; //默认第一个小点为白色
+
+		//clone一份轮播选项
+		exp_list.innerHTML +=exp_list.innerHTML;
+		len =exp_list.children.lenght;
+		exp_list.css("width",w*len+'px');
+	$(".exp_list").addEventListener("touchstart",start);
+	$(".exp_list").addEventListener("touchmove",move);
+	$(".exp_list").addEventListener("touchend",end);*/
+
+	function start(ev){
+
+	}
+
+
+})
